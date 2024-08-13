a838f0e999ce8aab98aad617b5667aaa10e26f96
Lucas Bigardi <lucas.bigardi@hotmail.com>
2024-08-12 21:15:53 -0300

    Avanço no trabalho

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+const port = 3000;
+
+app.use(express.static('./Paginas'));
+
+app.get('/hello', (req, res) => {
+    res.send('Hello, World! 123');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}');
+});
